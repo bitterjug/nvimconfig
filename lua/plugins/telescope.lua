9162a4f52b0be1cdd95e2a1cc8513bec22c47c18
Mark <bitterjug@gmail.com>
2025-03-16 15:41:50 +0000

    Start find on ctrlp

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,7 @@
 return {
     'nvim-telescope/telescope.nvim',
     event = 'VimEnter',
+    cmd = 'Telescope',
     branch = '0.1.x',
     dependencies = {
         'nvim-lua/plenary.nvim',
@@ -81,6 +82,7 @@ return {
 
         -- See `:help telescope.builtin`
         local builtin = require 'telescope.builtin'
+        vim.keymap.set('n', '<c-p>', builtin.find_files, { desc = '[S]earch [F]iles' })
         vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
         vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
         vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
