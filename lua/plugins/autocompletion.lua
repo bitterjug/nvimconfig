33c6a4813a17397f4d1e8ea7758d6d8f0b44505c
Mark Skipper <mark.skipper@featurespace.co.uk>
2025-03-21 10:02:05 +0000

    Completion mappings

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -81,9 +81,10 @@ return { -- Autocompletion
             -- No, but seriously. Please read `:help ins-completion`, it is really good!
             mapping = cmp.mapping.preset.insert {
                 -- Select the [n]ext item
-                ['<C-n>'] = cmp.mapping.select_next_item(),
+                ['<C-j>'] = cmp.mapping.select_next_item(),
                 -- Select the [p]revious item
-                ['<C-p>'] = cmp.mapping.select_prev_item(),
+                ['<C-k>'] = cmp.mapping.select_prev_item(),
+                ['<C-l>'] = cmp.mapping.confirm { select = true },
 
                 -- Scroll the documentation window [b]ack / [f]orward
                 ['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -113,16 +114,16 @@ return { -- Autocompletion
                 --
                 -- <c-l> will move you to the right of each of the expansion locations.
                 -- <c-h> is similar, except moving you backwards.
-                ['<C-l>'] = cmp.mapping(function()
-                    if luasnip.expand_or_locally_jumpable() then
-                        luasnip.expand_or_jump()
-                    end
-                end, { 'i', 's' }),
-                ['<C-h>'] = cmp.mapping(function()
-                    if luasnip.locally_jumpable(-1) then
-                        luasnip.jump(-1)
-                    end
-                end, { 'i', 's' }),
+                -- ['<C-l>'] = cmp.mapping(function()
+                --     if luasnip.expand_or_locally_jumpable() then
+                --         luasnip.expand_or_jump()
+                --     end
+                -- end, { 'i', 's' }),
+                -- ['<C-h>'] = cmp.mapping(function()
+                --     if luasnip.locally_jumpable(-1) then
+                --         luasnip.jump(-1)
+                --     end
+                -- end, { 'i', 's' }),
 
                 -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
                 --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
