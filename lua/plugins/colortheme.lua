bd50e9ecf231ba8105bb72f1a62eb97b0da9b4e0
Mark <bitterjug@gmail.com>
2025-03-22 20:09:08 +0000

    Diff colours

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,21 +1,42 @@
 return {
-    'f4z3r/gruvbox-material.nvim',
-    name = 'gruvbox-material',
-    lazy = false,
-    priority = 1000,
-    opts = {},
-    config = function()
-        require('gruvbox-material').setup {
-            contrast = 'hard',
-        }
-        local theme = require('gruvbox-material.lualine').theme 'medium'
-        -- some theme customization to your liking, for instance:
-        -- local g_colors = require("gruvbox-material.colors")
-        -- local colors = g_colors.get(vim.o.background, "medium")
-        -- theme.normal["x"] = { fg = colors.bg_statusline1, bg = colors.purple }
-        require('lualine').setup {
-            options = { theme = theme },
-        }
-        vim.cmd 'colorscheme gruvbox-material'
-    end,
+    {
+        'f4z3r/gruvbox-material.nvim',
+        name = 'gruvbox-material',
+        lazy = false,
+        priority = 1000,
+        opts = {},
+        config = function()
+            local colors = require('gruvbox-material.colors').get(vim.o.background, 'hard')
+            require('gruvbox-material').setup {
+                contrast = 'hard',
+                customize = function(group, options)
+                    if group == 'DiagnosticUnderlineError' then
+                        options.link = nil
+                        options.undercurl = true
+                        options.sp = '#703030'
+                    elseif group == 'DiffText' then
+                        options.link = nil
+                        options.underdashed = true
+                        options.fg = nil
+                        options.sp = colors.fg1
+                        options.bg = colors.bg_diff_blue
+                    elseif group == 'DiffDelete' then
+                        options.link = nil
+                        options.bg = '#181818'
+                    end
+                    return options
+                end,
+            }
+            local theme = require('gruvbox-material.lualine').theme 'medium'
+            -- some theme customization to your liking, for instance:
+            -- local g_colors = require("gruvbox-material.colors")
+            -- local colors = g_colors.get(vim.o.background, "medium")
+            -- theme.normal["x"] = { fg = colors.bg_statusline1, bg = colors.purple }
+            require('lualine').setup {
+                options = { theme = theme },
+            }
+            vim.cmd 'colorscheme gruvbox-material'
+        end,
+    },
+    { 'brenoprata10/nvim-highlight-colors', opts = {} },
 }
