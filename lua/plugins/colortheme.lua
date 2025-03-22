2471aa4858fbf028126f2bcbbb7c684dc075f38a
Mark <bitterjug@gmail.com>
2025-03-22 10:23:33 +0000

    Gruvbox

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -43,7 +43,19 @@ return {
             }
 
             -- setup must be called before loading
-            vim.cmd 'colorscheme kanagawa-dragon'
+            -- vim.cmd 'colorscheme kanagawa-dragon'
+        end,
+    },
+    { 'ellisonleao/gruvbox.nvim', priority = 1000, config = true, opts = {} },
+    {
+        'sainnhe/gruvbox-material',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            -- Optionally configure and load the colorscheme
+            -- directly inside the plugin declaration.
+            vim.g.gruvbox_material_enable_italic = true
+            vim.cmd.colorscheme 'gruvbox-material'
         end,
     },
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -47,7 +47,7 @@ return {
         require('lualine').setup {
             options = {
                 icons_enabled = true,
-                theme = 'nord',
+                theme = 'gruvbox_material',
                 -- Some useful glyphs:
                 -- https://www.nerdfonts.com/cheat-sheet
                 --        
