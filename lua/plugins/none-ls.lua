8fb0404383bbcc8899dfdf6616c729f88151b279
Mark Skipper <mark.skipper@featurespace.co.uk>
2025-03-24 08:58:23 +0000

    Format css

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -24,7 +24,9 @@ return {
 
         local sources = {
             diagnostics.checkmake,
-            formatting.prettier.with { filetypes = { 'html', 'json', 'yaml', 'markdown' } },
+            formatting.prettier.with {
+                filetypes = { 'html', 'json', 'yaml', 'markdown', 'scss', 'ts' },
+            },
             formatting.stylua,
             formatting.shfmt.with { args = { '-i', '4' } },
             formatting.elm_format,
