e62f0e553acdabb2e3790439c491df41f3628c30
Mark <bitterjug@gmail.com>
2025-04-06 10:22:05 +0100

    Add snippets for elixir

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -21,6 +21,8 @@ return { -- Autocompletion
                     'rafamadriz/friendly-snippets',
                     config = function()
                         require('luasnip.loaders.from_vscode').lazy_load()
+                        require('luasnip').filetype_extend('heex', { 'html' })
+                        require('luasnip').filetype_extend('elixir', { 'html' })
                     end,
                 },
             },
